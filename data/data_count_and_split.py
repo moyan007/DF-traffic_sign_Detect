fb4965247375a87ea2f8d81013f506fce054a37f
moyan007 <43445422+moyan007@users.noreply.github.com>
2019-06-06 15:34:34 +0800

    Update data_count_and_split.py

--- data/data_count_and_split.py
+++ data/data_count_and_split.py
@@ -4,8 +4,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import shutil
 #raw labels
-"""这里的data_train.csv文件是抽取了原来训练文件中的X1 Y1 X3 Y3以及filename和type之后的文件，
-需要剔除两个label有问题的文件，当然也可以继续剔除其他觉得有问题的label"""
+"""这里的data_train.csv文件是抽取了训练文件中的X1 Y1 X3 Y3以及filename和type之后的文件，
+需要剔除两个label有问题的文件，当然也可以继续剔除其他觉得有问题的label，注意，这里的文件名及框的坐标都是裁剪后的命名及框结果"""
 raw_labels = pd.read_csv("data_train.csv",header=None).values
 print(raw_labels.shape)#(20238, 6)
 filename = []
